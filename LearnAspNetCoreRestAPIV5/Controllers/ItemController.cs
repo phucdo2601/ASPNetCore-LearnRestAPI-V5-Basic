87c32582320fb9205435d39c02a04d2c3a7135c5
phucdo2601 <ngocphucdo2601@gmail.com>
2022-04-11 19:11:00 +0700

    First Learn dotnet5 rest api basic: entity, repository, controller b01

---
+++ LearnAspNetCoreRestAPIV5/Controllers/ItemController.cs
@@ -0,0 +1,42 @@
+﻿using LearnAspNetCoreRestAPIV5.Entities;
+using LearnAspNetCoreRestAPIV5.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LearnAspNetCoreRestAPIV5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly InMemItemRepository repository;
+
+        public ItemController()
+        {
+            repository = new InMemItemRepository();
+        }
+
+        //GET / items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        //Get /items/Id
+        [HttpGet("{Id}")]
+        public ActionResult<Item> GetItemById(Guid Id)
+        {
+            var item = repository.GetItemById(Id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+    }
+}
